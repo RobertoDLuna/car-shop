26c245aaaea127ca094c34c0d14ba580ce04af39
Roberto Luna <robertocgw@gmail.com>
2022-11-20 16:52:52 -0300

    adição dos endpoints

--- src/Routes/car.routes.ts
+++ src/Routes/car.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import CarController from '../Controllers/car.controller';
 
 const carRoute = Router();
 
 carRoute.post('/cars', (req, res, next) => new CarController(req, res, next).create());
+carRoute.get('/cars', (req, res, next) => new CarController(req, res, next).showAll());
 
 export default carRoute;
