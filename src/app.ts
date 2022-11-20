22b8aaaf1b3ff9d6dabe5a51d4ba4519ec94b0e3
Roberto Luna <robertocgw@gmail.com>
2022-11-20 17:02:51 -0300

    feat: Requisito 1 - Crie a rota /cars onde seja possível cadastrar um carro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import ErrorHandler from './Middlewares/ErrorMiddleware';
+import carRoute from './Routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(carRoute);
+app.use(ErrorHandler.handle);
+
+app.get('/', async (req: Request, res: Response) => res.status(200).json({ message: 'Conectado' }));
 
 export default app;
