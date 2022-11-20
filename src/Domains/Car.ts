c1a6f137ddbbbe81bf8f8f5bbb1cbdc5cad2bb7d
Roberto Luna <robertocgw@gmail.com>
2022-11-20 15:51:17 -0300

    adição do Domain Car

---
+++ src/Domains/Car.ts
@@ -0,0 +1,79 @@
+import Icar from '../Interfaces/ICar';
+
+export default class Car {
+  protected id?: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor({ 
+    id,
+    model,
+    year,
+    color,
+    buyValue,
+    seatsQty,
+    doorsQty,
+    status = false }: ICar) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+}
