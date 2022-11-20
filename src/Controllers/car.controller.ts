b349327cd6624053abb7fc07029ee145e789d1c0
Roberto Luna <robertocgw@gmail.com>
2022-11-20 16:46:57 -0300

    criação e adição de funções no car controller

---
+++ src/Controllers/car.controller.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import CarService from '../Services/car.service';
+
+export default class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new CarService();
+  }
+
+  public async create() {
+    try {
+      const car = await this.service.create(this.req.body);
+      this.res.status(201).json(car);
+    } catch (err) {
+      this.next(err);
+    }
+  }
+
+  public async showAll() {
+    try {
+      const allCars = await this.service.getAll();
+      this.res.status(200).json(allCars);
+    } catch (err) {
+      this.next(err);
+    }
+  }
+}
