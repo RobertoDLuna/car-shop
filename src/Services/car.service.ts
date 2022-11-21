760a037c46bfa44b0552514479dcffeea2aad60c
Roberto Luna <robertocgw@gmail.com>
2022-11-21 10:57:16 -0300

    adição da findById no service de car

--- src/Services/car.service.ts
+++ src/Services/car.service.ts
@@ -1,6 +1,7 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarsODM';
+import CustomizedError from '../Utils/CustomizedError';
 
 export default class CarService {
   private carODM: CarODM;
@@ -16,5 +17,11 @@ export default class CarService {
   public async create(car: ICar) {
     const newCar = await this.carODM.create(car);
     return new Car(newCar);
+  }
+
+  public async findById(id: string) {
+    const car = await this.carODM.getById(id);
+    if (!car) throw new CustomizedError('Car not found', 404);
+    return new Car(car);
   }
 }
